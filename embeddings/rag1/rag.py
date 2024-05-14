9d9ec6a8927c132abd50c483501429c0b3cdd4d4
Joao Pedro Schmitt <schmittjoaopedro@gmail.com>
2024-05-14 12:38:52 -0700

    code

--- embeddings/rag1/rag.py
+++ embeddings/rag1/rag.py
@@ -4,41 +4,48 @@ from textwrap import wrap
 import pandas as pd
 import numpy as np
 
-# https://huggingface.co/datasets/MongoDB/embedded_movies
-dataset = load_dataset("MongoDB/embedded_movies")
-dataset_df = pd.DataFrame(dataset['train'])
-print(dataset_df.info())
-
 # Models: gte-small, gte-base, gte-large
 embedding_model = SentenceTransformer("thenlper/gte-large")
-counter = 0
-stop_at = 10  # dataset_df.shape[0]
-dataset_df = dataset_df.head(stop_at)
 
 
-def get_embedding(text: str) -> list[float]:
-    global counter
-    counter += 1
-    if counter % 10 == 0:
-        print(f"Processed {counter} texts.")
-    if not text or not text.strip():
+def get_embedding(content: str) -> list[float]:
+    if not content or not content.strip():
         print("Attempted to get embedding for empty text.")
         return []
 
-    embedding = embedding_model.encode(text)
+    embedding = embedding_model.encode(content)
     return embedding.tolist()
 
 
-dataset_df["embedding"] = dataset_df["fullplot"].apply(get_embedding)
-# Print the columns fullplot and embedding for the first 5 rows
-print(dataset_df[["fullplot", "embedding"]].head())
-
-
-def similarity(query_embedding, text_embedding):
-    similarity = np.dot(text_embedding, query_embedding) / (np.linalg.norm(text_embedding) * np.linalg.norm(query_embedding))
-    distance = np.linalg.norm(np.array(query_embedding) - np.array(text_embedding))
+def calculate_similarity(query_emb, response_emb):
+    if not response_emb:
+        similarity = 0
+        distance = 1
+    else:
+        similarity = np.dot(response_emb, query_emb) / (np.linalg.norm(response_emb) * np.linalg.norm(query_emb))
+        distance = np.linalg.norm(np.array(query_emb) - np.array(response_emb))
     return similarity, distance
 
+
+# If pre-processed CSV file already exists
+try:
+    dataset_df = pd.read_csv("movies.csv")
+    dataset_df["embedding"] = dataset_df["embedding"].apply(eval)
+except FileNotFoundError:
+    dataset_df = pd.DataFrame()
+
+if dataset_df.empty:
+    # https://huggingface.co/datasets/MongoDB/embedded_movies
+    dataset = load_dataset("MongoDB/embedded_movies")
+    dataset_df = pd.DataFrame(dataset['train'])
+    print(dataset_df.info())
+
+    dataset_df["embedding"] = dataset_df["fullplot"].apply(get_embedding)
+    dataset_df = dataset_df[["fullplot", "embedding"]]
+    print(dataset_df.head())
+
+    # Save the dataset to a CSV file
+    dataset_df.to_csv("movies.csv", index=False)
 
 # Ask user for a search query
 text = input("Search for movies: ")
@@ -49,7 +56,7 @@ if text:
     docs_similarity = []
     for i, row in dataset_df.iterrows():
         text_embedding = row["embedding"]
-        sim, dist = similarity(query_embedding, text_embedding)
+        sim, dist = calculate_similarity(query_embedding, text_embedding)
         docs_similarity.append({
             "fullplot": row["fullplot"],
             "similarity": sim,
